3dc6c52591ea31300e41950b6eb7aa09b08aed4f
Maximilian Niessl <contact@maxniessl.com>
2022-05-21 00:41:15 +0200

    Changed to synchronised function

--- commands/list.js
+++ commands/list.js
@@ -23,54 +23,48 @@ module.exports = {
 
         // load items.json
         const fs = require('fs');
-        itemFile = fs.readFile('res/items.json', 'utf-8', (err, jsonString) => {
-            if (err) {
-                console.log(err);
-            } else {
-                try {
-                    const itemData = JSON.parse(jsonString);
+        const itemFile = fs.readFileSync('res/items.json', 'utf-8');
+        try {
+            const itemData = JSON.parse(itemFile);
 
-                    // list all groups
-                    for (var i = 0; i < itemData.groups.length; i++) {
+            // list all groups
+            for (var i = 0; i < itemData.groups.length; i++) {
 
-                        var itemGroup = itemData.groups[i];
+                var itemGroup = itemData.groups[i];
 
-                        // only display group from passed argument
-                        if (argumentGroup !== "" && argumentGroup !== itemGroup.name) {
-                            continue;
-                        }
-
-                        const embed = new MessageEmbed()
-                            .setTitle(itemGroup.name)
-                            .setDescription("List of items in group `" + itemGroup.name + "`")
-                            .setColor("#5699c7")
-                            .setTimestamp();
-                        
-                        // list all items
-                        for (var j = 0; j < itemGroup.items.length; j++) {
-
-                            var item = itemGroup.items[j];
-
-                            var variantString = "";
-
-                            // list all variants
-                            for (var k = 0; k < item.variants.length; k++) {
-
-                                var variant = item.variants[k];
-
-                                variantString = variantString + "• " + variant.name + " `" + variant.price + "$`" + "\n";
-                            }
-                            
-                            embed.addField(item.name, variantString, true);
-                        }
-                        
-                        message.channel.send({ embeds: [embed] });
-                    }
-                } catch (err) {
-                    console.log(err);
+                // only display group from passed argument
+                if (argumentGroup !== "" && argumentGroup !== itemGroup.name) {
+                    continue;
                 }
+
+                const embed = new MessageEmbed()
+                    .setTitle(itemGroup.name)
+                    .setDescription("List of items in group `" + itemGroup.name + "`")
+                    .setColor("#5699c7")
+                    .setTimestamp();
+                
+                // list all items
+                for (var j = 0; j < itemGroup.items.length; j++) {
+
+                    var item = itemGroup.items[j];
+
+                    var variantString = "";
+
+                    // list all variants
+                    for (var k = 0; k < item.variants.length; k++) {
+
+                        var variant = item.variants[k];
+
+                        variantString = variantString + "• " + variant.name + " `" + variant.price + "$`" + "\n";
+                    }
+                    
+                    embed.addField(item.name, variantString, true);
+                }
+                
+                message.channel.send({ embeds: [embed] });
             }
-        });
+        } catch (err) {
+            console.log(err);
+        }
     }
 }
-  
